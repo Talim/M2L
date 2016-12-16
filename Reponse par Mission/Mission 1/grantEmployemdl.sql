375d8c70c7275cb48dee03f07e349f582e98263e
Unknown <dylan_83000@hotmail.fr>
2016-12-16 18:51:02 +0100

    Grant Employemdl

---
+++ Reponse par Mission/Mission 1/grantEmployemdl.sql
@@ -0,0 +1,5 @@
+GRANT EXECUTE ON mdl.GererAtelier TO employemdl;
+GRANT EXECUTE ON mdl.GererTheme TO employemdl;
+GRANT EXECUTE ON mdl.GererVacation TO employemdl;
+GRANT SELECT, INSERT, UPDATE, DELETE ON atelier TO employemdl;
+GRANT SELECT, INSERT, UPDATE, DELETE ON vacation TO employemdl;
