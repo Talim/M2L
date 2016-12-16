6e2855344c3c77600ae6abe567cc31e94626013a
Unknown <dylan_83000@hotmail.fr>
2016-12-16 19:09:54 +0100

    Mission 3 : Interface de base A MODIFIER

---
+++ Projet/M2L_Mission3/M2L_Mission3/Inscription.cs
@@ -0,0 +1,27 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M2L_Mission3
+{
+    public partial class Inscription : MaterialForm
+    {
+        private readonly MaterialSkinManager materialSkinManager;
+        public Inscription()
+        {
+            InitializeComponent();
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+    }
+}
